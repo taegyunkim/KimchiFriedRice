75f817ec66ad7b1e2b8121b51fba4a72a30f7df7
Taegyun Kim <k.taegyun@gmail.com>
2020-02-20 14:13:33 -0500

    Initial greedy implementation.

---
+++ books/src/main.rs
@@ -0,0 +1,234 @@
+use std::collections::{HashMap, HashSet};
+use std::env;
+use std::fs::File;
+use std::io::prelude::*;
+use std::io::BufReader; // 1.2.7
+
+#[derive(Debug, PartialEq, Eq, Copy, Clone)]
+enum State {
+    SignedUp,
+    InProcess(usize),
+    NeedsSignUp,
+}
+
+#[derive(Debug)]
+struct Library {
+    idx: usize,
+    state: State,
+    days_to_signup: usize,
+    ship_rate: usize,
+    books: Vec<usize>,
+
+    sent: Vec<usize>,
+}
+
+impl Library {
+    fn new(idx: usize, days_to_signup: usize, ship_rate: usize, books: Vec<usize>) -> Self {
+        Self {
+            idx,
+            state: State::NeedsSignUp,
+            days_to_signup,
+            ship_rate,
+            books,
+            sent: Vec::new(),
+        }
+    }
+
+    fn ship(&mut self, scanned: &mut HashSet<usize>) {
+        let mut cnt = 0;
+
+        while self.books.len() > 0 && cnt < self.ship_rate {
+            let book = self.books.pop().unwrap();
+            if scanned.contains(&book) {
+                continue;
+            }
+            // Also add this book to the scanned set to prevent other libraries from sending the
+            // same book.
+            scanned.insert(book);
+            self.sent.push(book);
+            cnt += 1;
+        }
+    }
+
+    fn process(&mut self, scanned: &mut HashSet<usize>) {
+        match self.state {
+            State::SignedUp => self.ship(scanned),
+            State::InProcess(0) => {
+                self.state = State::SignedUp;
+                self.ship(scanned);
+            }
+            State::InProcess(days) => {
+                self.state = State::InProcess(days - 1);
+            }
+            State::NeedsSignUp => {}
+        }
+    }
+
+    fn state(&self) -> State {
+        self.state
+    }
+
+    fn signup(&mut self) {
+        self.state = State::InProcess(self.days_to_signup);
+    }
+
+    fn days_to_signup(&self) -> usize {
+        self.days_to_signup
+    }
+
+    fn index(&self) -> usize {
+        self.idx
+    }
+
+    fn sent(&self) -> &[usize] {
+        &self.sent
+    }
+}
+
+fn signup_one_lib(libraries: &mut Vec<Library>) -> Option<usize> {
+    for library in libraries {
+        if library.state() == State::NeedsSignUp {
+            library.signup();
+            return Some(library.index());
+        }
+    }
+    return None;
+}
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+
+    if args.len() != 2 {
+        println!("Please specify the data file to read.");
+        std::process::exit(-1);
+    }
+
+    let filename = &args[1];
+    let f = File::open(filename).expect("Failed to open file.");
+    let mut reader = BufReader::new(f);
+
+    let mut line = String::new();
+
+    reader
+        .read_line(&mut line)
+        .expect("Failed to parse the first line.");
+
+    let first_line = line
+        .trim()
+        .split(' ')
+        .flat_map(str::parse::<usize>)
+        .collect::<Vec<_>>();
+    line.clear();
+
+    let b = first_line[0];
+    let l = first_line[1];
+    let d = first_line[2];
+    #[cfg(debug_assertions)]
+    println!("{} {} {}", b, l, d);
+
+    reader
+        .read_line(&mut line)
+        .expect("Failed to parse the second line.");
+    let book_score_list = line
+        .trim()
+        .split(' ')
+        .flat_map(str::parse::<u32>)
+        .collect::<Vec<_>>();
+    line.clear();
+
+    #[cfg(debug_assertions)]
+    println!("{:?}", book_score_list);
+
+    assert_eq!(book_score_list.len(), b);
+
+    let mut books_to_scores: HashMap<usize, u32> = HashMap::new();
+    for (idx, score) in book_score_list.iter().enumerate() {
+        books_to_scores.insert(idx, *score);
+    }
+
+    let mut libraries = Vec::with_capacity(l as usize);
+
+    for lib_idx in 0..l as usize {
+        // Parse the first line of library lib_idx
+        reader
+            .read_line(&mut line)
+            .expect("failed to parse library first line.");
+
+        let lib_header = line
+            .trim()
+            .split(' ')
+            .flat_map(str::parse::<usize>)
+            .collect::<Vec<_>>();
+        let n = lib_header[0];
+        let t = lib_header[1];
+        let m = lib_header[2];
+
+        #[cfg(debug_assertions)]
+        println!("{} {} {}", n, t, m);
+
+        line.clear();
+
+        // Parse the second line of lbirary lib_idx
+        reader
+            .read_line(&mut line)
+            .expect("failed to parse library second line.");
+
+        let mut books = line
+            .trim()
+            .split(' ')
+            .flat_map(str::parse::<usize>)
+            .collect::<Vec<_>>();
+        assert_eq!(books.len(), n);
+
+        // Sort the books in decreasing order of the scores.
+        books.sort_by(|a, b| {
+            books_to_scores
+                .get(b)
+                .unwrap()
+                .partial_cmp(books_to_scores.get(a).unwrap())
+                .unwrap()
+        });
+
+        #[cfg(debug_assertions)]
+        println!("{:?}", books);
+
+        line.clear();
+
+        libraries.push(Library::new(lib_idx, t, m, books));
+    }
+
+    libraries.sort_by(|a, b| a.days_to_signup().partial_cmp(&b.days_to_signup()).unwrap());
+
+    let mut scanned: HashSet<usize> = HashSet::new();
+
+    let mut signed: Vec<usize> = Vec::new();
+    for _ in 0..d {
+        if let Some(idx) = signup_one_lib(&mut libraries) {
+            signed.push(idx);
+        }
+
+        for library in &mut libraries {
+            library.process(&mut scanned);
+        }
+    }
+
+    println!("{}", signed.len());
+
+    for idx in signed {
+        for library in &libraries {
+            if library.index() == idx {
+                println!("{} {}", idx, library.sent().len());
+
+                println!(
+                    "{}",
+                    library
+                        .sent()
+                        .iter()
+                        .map(|n| n.to_string())
+                        .collect::<Vec<String>>()
+                        .join(" ")
+                );
+            }
+        }
+    }
+}
